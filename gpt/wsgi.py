9467df1c27b1776e9b21102d9e3609d2b9154a9d
Snk_b <48286888+Snkb-ch@users.noreply.github.com>
2023-04-15 13:57:56 +0300

    fix

--- gpt/wsgi.py
+++ gpt/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 
 import os, sys
-sys.path.insert(0, '/var/www/u2019785/data/www/gpt.ru/gpt')
+sys.path.insert(0, '/var/www/u2019785/data/www/brainstormai.ru/gpt')
 sys.path.insert(1, '/var/www/u2019785/data/djangoenv/lib/python3.7/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'gpt.settings'
 from django.core.wsgi import get_wsgi_application
