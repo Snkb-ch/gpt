09cb4dd30b5a0ee1f24450d4992d543236f7baaa
Snk_b <48286888+Snkb-ch@users.noreply.github.com>
2023-04-20 21:38:51 +0300

    Update settings.py

--- gpt/settings.py
+++ gpt/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'gpt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # DATABASES = {
