9379a83c828e53e461e86689750450493a89da56
Snk_b <48286888+Snkb-ch@users.noreply.github.com>
2023-04-22 17:08:17 +0300

    Update views.py

--- basegpt/views.py
+++ basegpt/views.py
@@ -1,7 +1,7 @@
 import json
 import traceback
 
-import awaitable as awaitable
+
 from asgiref.sync import sync_to_async
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
