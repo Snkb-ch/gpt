1914e69aeeba532fbae21af5ea9d09ec0ab3b36b
Snk_b <48286888+Snkb-ch@users.noreply.github.com>
2024-05-11 15:34:31 +0300

    Update openai_helper.py

--- bot-script/openai_helper.py
+++ bot-script/openai_helper.py
@@ -252,7 +252,7 @@ class OpenAIHelper:
 
         else:
             logging.error(f"Error generating response: {response}")
-            yield 'Произошла ошибка при обработке запроса, приносим извенения', 0
+            yield 'Бот пока не работает — готовим обновление. Спасибо за терпение', 0
 
     # @retry(
     #     reraise=True,
