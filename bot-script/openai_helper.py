7ced54e16c0e627edc7156aa9168ced19b32e39c
Snk_b <48286888+Snkb-ch@users.noreply.github.com>
2024-09-12 21:32:09 +0300

    Update openai_helper.py

--- bot-script/openai_helper.py
+++ bot-script/openai_helper.py
@@ -45,7 +45,7 @@ from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_t
 # Models can be found here: https://platform.openai.com/docs/models/overview
 GPT_3_MODELS = ('gpt-3.5-turbo-1106', "gpt-3.5-turbo", "gpt-3.5-turbo-0613","gpt-3.5-turbo-1106" , "gpt-3.5-turbo-0125")
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613" , "gpt-3.5-turbo-0125", "gpt-3.5-turbo")
-GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613", "gpt-4-1106-preview", "gpt-4-vision-preview", "gpt-4-turbo","gpt-4-turbo-2024-04-09", "gpt-4o", "gpt-4o-mini")
+GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613", "gpt-4-1106-preview", "gpt-4-vision-preview", "gpt-4-turbo","gpt-4-turbo-2024-04-09", "gpt-4o", "gpt-4o-mini", "gpt-4o-2024-08-06")
 GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
 LLAMA_MODELS_70= ("meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo", )
 LLAMA_MODELS_400= ("meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo", )
